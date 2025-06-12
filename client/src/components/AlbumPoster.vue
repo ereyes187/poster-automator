68b4be95ad402d13ae1d60e424500efc9599397a
Ernesto Reyes <ereyes20981@gmail.com>
2025-06-11 17:14:32 -0700

    integrated color thief to extract album color palette for dynamic poster theming

--- client/src/components/AlbumPoster.vue
+++ client/src/components/AlbumPoster.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { defineProps, ref, watchEffect } from "vue";
 import type { AlbumObject } from "@/interfaces";
+import ColorThief from "colorthief";
 
 const props = defineProps<{
   album: AlbumObject;
 }>();
 
 let albumLength = ref<string>();
-// let tracksStringLength = ref<number>(0);
+let image = ref<HTMLImageElement>();
+let primaryColor = ref<string>();
+let secondaryColor = ref<string>();
+let tertiaryColor = ref<string>();
+let readableTextColor = ref<string>();
 
 watchEffect(() => {
   let totalMinutes = 0;
@@ -21,22 +26,46 @@ watchEffect(() => {
   albumLength.value = length;
 });
 
-// watchEffect(() => {
-//   let length = 0;
-//   for (let i = 0; i < props.album.tracks.items.length; i++) {
-//     length = length + props.album.tracks.items[i].name.length;
-//   }
-//   tracksStringLength.value = length;
-//   console.log(tracksStringLength.value);
-// });
+function onImgLoad() {
+  const colorthief = new ColorThief();
+  const colors = colorthief.getPalette(image.value, 3);
+  readableTextColor.value = getReadableTextColor(
+    colors[0][0],
+    colors[0][1],
+    colors[0][2],
+  );
+  primaryColor.value = colors[0].join(", ");
+  secondaryColor.value = colors[1].join(", ");
+  tertiaryColor.value = colors[2].join(", ");
+}
+
+function getReadableTextColor(r: number, g: number, b: number) {
+  const yiq = r * 0.299 + g * 0.587 + b * 0.114;
+  return yiq > 186 ? "black" : "white";
+}
 </script>
 
 <template>
-  <div class="justify-self-center bg-black text-white shadow-xl">
+  <div
+    class="justify-self-center shadow-xl"
+    :style="{
+      backgroundColor: `rgb(${primaryColor})`,
+      color: readableTextColor,
+    }"
+  >
     <div class="relative">
-      <img class="max-w-lg" :src="props.album.images[0].url" />
+      <img
+        ref="image"
+        class="max-w-lg"
+        @load="onImgLoad"
+        :src="props.album.images[0].url"
+        crossorigin="anonymous"
+      />
       <div
-        class="absolute bottom-0 flex h-60 w-full items-end justify-between bg-gradient-to-t from-[black] to-transparent pb-4"
+        class="absolute bottom-0 flex h-60 w-full items-end justify-between pb-4"
+        :style="{
+          background: `linear-gradient(to top, rgb(${primaryColor}), rgba(0,0,0,0))`,
+        }"
       >
         <hgroup class="pl-6 text-left text-lg">
           <h1 class="font-bold">
@@ -47,23 +76,44 @@ watchEffect(() => {
           </p>
         </hgroup>
         <div class="flex gap-2 pr-6">
-          <div class="h-10 w-10 border-2 border-white bg-neutral-950" />
-          <div class="h-10 w-10 border-2 border-white bg-neutral-700" />
-          <div class="h-10 w-10 border-2 border-white bg-neutral-500" />
+          <div
+            class="h-10 w-10 border-2"
+            :style="{
+              backgroundColor: `rgb(${primaryColor})`,
+              borderColor: readableTextColor,
+            }"
+          />
+          <div
+            class="h-10 w-10 border-2"
+            :style="{
+              backgroundColor: `rgb(${secondaryColor})`,
+              borderColor: readableTextColor,
+            }"
+          />
+          <div
+            class="h-10 w-10 border-2"
+            :style="{
+              backgroundColor: `rgb(${tertiaryColor})`,
+              borderColor: readableTextColor,
+            }"
+          />
         </div>
       </div>
     </div>
-    <hr class="mx-auto h-1 w-[94%] rounded-sm border-0 bg-white" />
-    <div class="space-between flex max-h-56 min-h-56 px-6 py-4">
+    <hr
+      class="mx-auto h-1 w-[94%] rounded-sm border-0"
+      :style="{ backgroundColor: readableTextColor }"
+    />
+    <div class="space-between flex max-h-56 min-h-56 px-6 py-4 text-[10px]">
       <ol class="flex flex-grow flex-col flex-wrap">
         <li
           v-for="(song, index) in props.album.tracks.items"
-          class="line-clamp-2 max-w-32 text-left text-[10px]"
+          class="line-clamp-2 max-w-32 text-left"
         >
           {{ index + 1 + ". " + song.name }}
         </li>
       </ol>
-      <div class="flex flex-col gap-4 text-[10px]">
+      <div class="flex flex-col gap-4">
         <hgroup class="text-right">
           <h1 class="pb-1 font-bold">Release Date</h1>
           <p>{{ props.album.release_date }}</p>
